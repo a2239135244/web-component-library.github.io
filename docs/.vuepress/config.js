6ba35e325dad248cacc31e9b55430d7d27ec5dcd
刘宇青 <yqliu25@iflytek.com>
2023-04-11 10:21:24 +0800

    📃 docs: 配置更改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 module.exports = {
-  base: '/',
+  base: '/web-component-library.github.io/',
   title: '',
   description: 'Just playing around',
   cache: false,
